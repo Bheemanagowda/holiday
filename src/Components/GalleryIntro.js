fb215c6e6f9cebeed1d2174357b635b7141fc7cf
Bheemanagowda <bheemanagowda07@mail.com>
2026-01-19 11:27:05 +0530

    Gallery image path changes

--- src/Components/GalleryIntro.js
+++ src/Components/GalleryIntro.js
@@ -1,5 +1,6 @@
 "use client";
 import React from "react";
+import Image from "next/image";
 import { Autoplay, FreeMode, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { v4 as uuidv4 } from "uuid";
@@ -54,12 +55,14 @@ function GalleryIntro({ images }) {
       >
         {images.map((image) => (
           <SwiperSlide key={uuidv4()} className="md:mb-16">
-            <img
-              src={image}
-              alt="gallery"
-              loading="lazy"
-              className="w-full lg:max-w-[520px] h-[350px] bg-gray-300 lg:h-[290px] xl:max-w-[440px] xl:h-[350px]  object-cover"
-            />
+           <Image
+  src={image} // your image path, e.g., "/images/gallery1.webp"
+  alt="gallery"
+  width={520}  // approximate width for large screens
+  height={350} // approximate height
+  className="w-full lg:max-w-[520px] h-[350px] bg-gray-300 lg:h-[290px] xl:max-w-[440px] xl:h-[350px] object-cover"
+  priority={false} // lazy loading by default; true if you want to load immediately
+/>
           </SwiperSlide>
         ))}
       </Swiper>
