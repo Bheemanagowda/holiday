6f0e5373c3aee8c85f747ec5b63ef4655b5e456e
Bheemanagowda <bheemanagowda07@mail.com>
2026-02-04 16:28:24 +0530

    changes updated

--- src/app/duniya-dekho/page.js
+++ src/app/duniya-dekho/page.js
@@ -2,16 +2,16 @@ import React from "react";
 import DuniyaDekhoIntro from "../../data/DuniyaDekhoIntro.json";
 import DestinationsPageIntro from "@/Components/ReuseComponent/DestinationsPageIntro";
 import LatestTour from "@/Components/ReuseComponent/LatestTour";
-import BharatDekhoLatestTour from "../../data/BharatDekhoLatestTour.json";
+import DuniyaDekhoLatestTour from "../../data/DuniyaDekhoLatestTour.json";
 
 
  export const metadata={
-  title: "Bharat Dekho - Vision Fly Holidays",
-  description:"Explore the beauty of India with Bharat Dekho tours. Discover diverse cultures, stunning landscapes, and vibrant traditions. Book your unforgettable journey today!"
+  title: "Dunia Dekho - Vision Fly Holidays",
+  description:"Explore the beauty of world with Dunia Dekho  tours. Discover diverse cultures, stunning landscapes, and vibrant traditions. Book your unforgettable journey today!"
  }
 function page() {
   return (
-    <div className="my-24 w-11/12 mx-auto">
+    <div className="my-24 w-full px-4 lg:w-11/12 lg:mx-auto overflow-x-hidden">
       <div className="text-center">
         {" "}
         <h1 className="font-secondary_font font-medium text-2xl lg:text-4xl xl:text-5xl">
@@ -22,12 +22,12 @@ function page() {
         </p>
       </div>
       <div className="lg:flex items-start gap-5 mt-12">
-        <div className="lg:w-[78%]">
+        <div className="lg:w-[74%]">
           {" "}
           <DestinationsPageIntro destinations={DuniyaDekhoIntro} />
         </div>
-        <div className="lg:w-[22%] mt-10 lg:mt-0">
-          <LatestTour latestTours={BharatDekhoLatestTour} />
+        <div className="lg:w-[26%] mt-10 lg:mt-0">
+          <LatestTour latestTours={DuniyaDekhoLatestTour} />
 
           <div className="text-[#343434] text-lg  border border-gray-500 rounded-lg px-4 py-6 mt-12">
             <h2 className="font-medium text-3xl pb-2">Need Help?</h2>
