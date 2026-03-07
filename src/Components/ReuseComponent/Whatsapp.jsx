0d980bee83bc791472c300a88e0c72bf4c0a2ce3
Bheemanagowda <bheemanagowda07@mail.com>
2026-03-07 16:23:58 +0530

    content updated correctly

--- src/Components/ReuseComponent/Whatsapp.jsx
+++ src/Components/ReuseComponent/Whatsapp.jsx
@@ -16,7 +16,7 @@ function Whatsapp() {
     p-2 bg-[#1FB141] rounded-full text-[32px] cursor-pointer z-50
     hover:scale-105 transition ease-in-out drop-shadow-2xl
 
-    hidden md:flex fixed right-4 bottom-[300px]
+    hidden md:flex fixed right-4 bottom-[200px]
   "
   onClick={handleClick}
 >
